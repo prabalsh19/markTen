3873eebba16b190091ddb685fb75f5ea147e3957
prabalsh19 <prabal.sharma2003@gmail.com>
2022-10-28 12:32:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ let cashGiven = document.querySelector("#cash-given")
 const btn = document.querySelector(".check-btn")
 const noOfNotes = document.querySelectorAll(".no-of-notes")
 
-const availableNotes = [2000,500,100,20,10,5,2]
+const availableNotes = [2000,500,100,20,10,5,1]
 const onClickHandler = ()=>{
    let billAmountValue = billAmount.value;
    let cashGivenValue = cashGiven.value;
 
    if(billAmountValue===''||cashGivenValue===''){
     document.querySelector(".error-message").innerText = "Invalid Input"
-   }else{
+   }else if(billAmountValue<0||cashGivenValue<0){
+    document.querySelector(".error-message").innerText = "Input Value cannot be negative"
+      
+   }
+   else if(billAmountValue===cashGivenValue){
+    document.querySelector(".error-message").innerText = "No change needed to be given"
+      
+   }
+   else{
     document.querySelector(".error-message").innerText = ""
     let extraAmount = cashGivenValue - billAmountValue;
     calculateAmount(extraAmount)
